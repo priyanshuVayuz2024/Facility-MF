0f1e94360b3a768e11f9fe5bde8730036df0f6ba
bhuvaneshVayuz <bhuvanesh.chaudhary@vayuz.com>
2025-12-09 16:27:47 +0530

    issues

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import * as MuiIcons from "@mui/icons-material";
 import * as ReactRouterDOM from "react-router-dom";
 import * as StylesMUI from "@mui/material/styles";
 import * as ReactHotToast from "react-hot-toast";
+import { getMuiTheme } from './theme/muiTheme.jsx'
 
 
 
@@ -107,14 +108,16 @@ const PrimaryLayout = () => {
 
 const AppWrapper = () => {
   return <>
-    <ReactRouterDOM.BrowserRouter>
-      <ReactRouterDOM.Routes>
-        <ReactRouterDOM.Route path='/' element={<PrimaryLayout />}>
-          <ReactRouterDOM.Route path='/*' element={<App />} />
-        </ReactRouterDOM.Route>
-      </ReactRouterDOM.Routes>
-    </ReactRouterDOM.BrowserRouter>
+    <StylesMUI.ThemeProvider theme={getMuiTheme()}>
 
+      <ReactRouterDOM.BrowserRouter>
+        <ReactRouterDOM.Routes>
+          <ReactRouterDOM.Route path='/' element={<PrimaryLayout />}>
+            <ReactRouterDOM.Route path='/*' element={<App />} />
+          </ReactRouterDOM.Route>
+        </ReactRouterDOM.Routes>
+      </ReactRouterDOM.BrowserRouter>
+    </StylesMUI.ThemeProvider>
   </>
 }
 
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,1 +1,32 @@
 export const basePath = localStorage?.getItem("facilitesMF") || "";
+export const uploadDocuments = async (files) => {
+    //   const formData = new FormData();
+
+    //   files.forEach((file) => {
+    //     formData.append("files", file); // 'files[]' matches the backend expectation
+    //   });
+
+    //   try {
+    //     const response = await tryCatch(() =>
+    //       axios.post("/api/upload-image", formData, {
+    //         headers: {
+    //           // "Content-Type": "multipart/form-data",
+    //           authorization: `Bearer ${getToken()}`, // hardcoded token
+    //         },
+    //       })
+    //     );
+
+    //     return response.data;
+    //   } catch (error) {
+    //     console.error("File upload failed:", error);
+    //     throw error;
+    //   }
+};
+
+
+export const stripHtml = (html) => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html;
+    const text = tempDiv.textContent || tempDiv.innerText || "";
+    return text.trim();
+};
