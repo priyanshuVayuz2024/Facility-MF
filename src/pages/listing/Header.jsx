b1d5c83cbb86bfb740b1a3792895c8926f3acab9
priyanshu <priyanshu.singh@vayuz.com>
2025-12-09 14:52:21 +0530

    icons and header

---
+++ src/pages/listing/Header.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { counts } from "../../components/dummyData";
+import { useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import { basePath } from "../../utils";
+import ticketIcon from "../../../public/icons/ticket-unchecked.svg";
+function Header() {
+  const navigate = useNavigate();
+  const formatCount = (count) => (count == undefined ? "--" : `${count}`);
+  const stats = [
+    {
+      label: "Active Bookings",
+      count: formatCount(counts?.active_booking),
+      link: `${basePath}/notice_board`,
+      icon: (
+        <img src="/icons/sheet.svg" />
+        // <LuFileSpreadsheet className="text-[#36AB6C] h-[20px] w-[20px]" />
+      ),
+      color: "bg-gradient-to-b from-[#E7F5ED] to-[#E6F4E6]",
+    },
+    {
+      label: "Upcoming Bookings",
+      count: formatCount(counts?.upcoming_booking),
+      link: `${basePath}/expired_notices`,
+      icon: (
+        <img src="/icons/alarm-clock-check.svg" />
+        // <LuAlarmClockCheck className="text-[#4D4D4F] h-[20px] w-[20px]" />
+      ),
+      color: "bg-[#F4F3F3]",
+    },
+    {
+      label: "Rejected Booking",
+      count: formatCount(counts?.rejected_booking),
+      link: `${basePath}/rejected-notices`,
+      icon: (
+        <img src={ticketIcon} />
+        //     // <LuTicketCheck className="text-[#AB0000] h-[20px] w-[20px]" />
+      ),
+      color: "bg-gradient-to-b from-[#F5E5E5] to-[#F4E7E7]",
+    },
+    {
+      label: "Pending Booking",
+      count: formatCount(counts?.pending_booking),
+      link: `${basePath}/pending_for_approval_notices`,
+      icon: (
+        <img src="/icons/triangle-alert.svg" />
+        // <LuTriangleAlert className="text-[#F27C15] h-[20px] w-[20px]" />
+      ),
+      color: "bg-gradient-to-b from-[#FDEFE3] to-[#FDEEE2]",
+    },
+  ];
+
+  return (
+    <div className="pt-5 pb-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      {stats.map((item, index) => {
+        return (
+          <div
+            key={item.label}
+            className={`${item.color} min-h-[92px] cursor-pointer flex flex-col items-start gap-3.5 px-4 py-4 border-[0.5px] border-[#EBEBEB] rounded-xl transition-all`}
+            onClick={() => {
+              const searchParams = new URLSearchParams(location.search);
+              const view = searchParams.get("view");
+              const viewParam = view ? `?view=${view}` : "";
+              navigate(`${item.link}${viewParam}`);
+            }}
+          >
+            <div className="w-full flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                {item.icon}
+                <div className="text-base font-normal leading-[20px] text-[#4D4D4F]">
+                  {item.label}
+                </div>
+              </div>
+              <IconButton className="!p-0">
+                <img src="/icons/arrow-right.svg" />
+                {/* <LuArrowRight className="text-[#884EA7]" /> */}
+              </IconButton>
+            </div>
+            <div className="text-xl font-semibold leading-[24px]">
+              {item.count.toString().padStart(2, "0")}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Header;
