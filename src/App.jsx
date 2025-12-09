d5cad840162f0b292e4f57c443ce1ed1e2a8924c
priyanshu <priyanshu.singh@vayuz.com>
2025-12-09 16:25:08 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import BasicDetails from "./pages/createFacility/BasicDetails";
 import TimeAvailability from "./pages/createFacility/TimeAvailability";
 import BookingRules from "./pages/createFacility/BookingRules";
 import FacilityListing from "./pages/listing";
+import FacilityLayout from "./components/ui/FacilityLayout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/facility" element={<FacilityListing />} />
-        <Route path="/create-facility" element={<CreateLayout />}>
-          <Route index element={<BasicDetails />} />
-          <Route path="basic-details" element={<BasicDetails />} />
-          <Route path="time-availability" element={<TimeAvailability />} />
-          <Route path="booking-rules" element={<BookingRules />} />
+        <Route element={<FacilityLayout />}>
+          <Route path="/facilities" element={<FacilityListing />} />
+          <Route path="/active_bookings" element={<FacilityListing />} />
+          <Route path="/upcoming_bookings" element={<FacilityListing />} />
+          <Route path="/rejected_bookings" element={<FacilityListing />} />
+          <Route path="/pending_bookings" element={<FacilityListing />} />
+
+          <Route path="/create-facility" element={<CreateLayout />}>
+            <Route index element={<BasicDetails />} />
+            <Route path="basic-details" element={<BasicDetails />} />
+            <Route path="time-availability" element={<TimeAvailability />} />
+            <Route path="booking-rules" element={<BookingRules />} />
+          </Route>
         </Route>
       </Routes>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode, useEffect, useMemo, useRef, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import MicrofrontendLoader from './MFloader/MicroFrontendLoader.jsx'
+import { StrictMode, useEffect, useMemo, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import MicrofrontendLoader from "./MFloader/MicroFrontendLoader.jsx";
 import React from "react";
 import * as MaterialUI from "@mui/material";
 import ReactDOM from "react-dom";
@@ -12,10 +12,7 @@ import * as MuiIcons from "@mui/icons-material";
 import * as ReactRouterDOM from "react-router-dom";
 import * as StylesMUI from "@mui/material/styles";
 import * as ReactHotToast from "react-hot-toast";
-
-
-
-
+import { getMuiTheme } from "./theme/muiTheme.jsx";
 
 const PrimaryLayout = () => {
   window.React = React;
@@ -29,26 +26,27 @@ const PrimaryLayout = () => {
   window.StylesMUI = StylesMUI;
   window.ReactHotToast = ReactHotToast;
 
-
   const [open, setOpen] = useState(true);
   const mfRef = useRef(null);
   const headerRef = useRef(null);
 
-
   const [headerLoaded, setHeaderLoaded] = useState(false);
   const [sidebarLoaded, setSidebarLoaded] = useState(false);
 
-
-  let routes = JSON.parse('[{"text":"NoticeBoard","containerId":"noticeBoardMF","url":" / notice - board","bundleLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/Noticeboard/1.1/mf/noticeboard-bundle.js","cssLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/Noticeboard/1.1/mf/noticeboardmf.css","additionalLink":[],"layout":true},{"text":"Login","containerId":"signInMF","url":"/sign-in/","bundleLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/login/1.1/mf/signin-bundle.js","cssLink":null,"group":{"_id":"687744be75039553643227d9","moduleGroupId":"moduleGroupId_1752646846085","moduleName":"Dashboard","moduleIcon":"AcUnit","category":"General"},"additionalLink":["https://d18aratlqkym29.cloudfront.net/frontend-build/login/1.1/mf/signin-bundle.js"],"layout":false}]')
-  let config = []
+  let routes = JSON.parse(
+    '[{"text":"NoticeBoard","containerId":"noticeBoardMF","url":" / notice - board","bundleLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/Noticeboard/1.1/mf/noticeboard-bundle.js","cssLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/Noticeboard/1.1/mf/noticeboardmf.css","additionalLink":[],"layout":true},{"text":"Login","containerId":"signInMF","url":"/sign-in/","bundleLink":"https://d18aratlqkym29.cloudfront.net/frontend-build/login/1.1/mf/signin-bundle.js","cssLink":null,"group":{"_id":"687744be75039553643227d9","moduleGroupId":"moduleGroupId_1752646846085","moduleName":"Dashboard","moduleIcon":"AcUnit","category":"General"},"additionalLink":["https://d18aratlqkym29.cloudfront.net/frontend-build/login/1.1/mf/signin-bundle.js"],"layout":false}]'
+  );
+  let config = [];
 
   // Only pass static props at initial mount
-  const stableProps = useMemo(() => ({
-    open,
-    setOpen,
-    setHeaderLoaded
-  }), []);
-
+  const stableProps = useMemo(
+    () => ({
+      open,
+      setOpen,
+      setHeaderLoaded,
+    }),
+    []
+  );
 
   // Update MF props when location changes
   useEffect(() => {
@@ -60,68 +58,66 @@ const PrimaryLayout = () => {
     }
   }, [open, routes, config]);
 
-
   const layoutBundlesLoaded = headerLoaded && sidebarLoaded;
 
-
-
-  return <>
-    <div className="fixed w-full z-10 bg-white">
-      <MicrofrontendLoader
-        ref={headerRef}
-        scriptUrl={
-          "https://d18aratlqkym29.cloudfront.net/frontend-build/header/1.1/mf/header-bundle.js?date=1765256423001"
-        }
-        mountDivId="headerMain"
-        globalVarName="headerMain"
-        propsToPass={stableProps}
-      />
-    </div>
-    <div className="flex bg-var(--color-primary)">
-      <MicrofrontendLoader
-        ref={mfRef}
-        scriptUrl={
-          "https://d18aratlqkym29.cloudfront.net/frontend-build/sidebar/1.1/mf/sidebar-bundle.js?date=1765256423001"
-        }
-        mountDivId="sidebarMain"
-        globalVarName="sidebarMain"
-        propsToPass={stableProps}
-        onLoad={() => setSidebarLoaded(true)}
-      />
-      <div className={`${!open ? "xl:max-w-[calc(100%-80px)]" : "xl:max-w-[calc(100%-240px)]"} relative min-h-[calc(100%-80px)]! p-4 xl:p-6 pt-[104px]! w-full bg-[#F5F9FE]`}>
-        {true ? (
-          <ReactRouterDOM.Outlet />
-        ) : (
-          <div className="fixed inset-0 bg-white z-40 flex items-center justify-center">
-            <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
-          </div>
-        )}
+  return (
+    <>
+      <div className="fixed w-full z-10 bg-white">
+        <MicrofrontendLoader
+          ref={headerRef}
+          scriptUrl={
+            "https://d18aratlqkym29.cloudfront.net/frontend-build/header/1.1/mf/header-bundle.js?date=1765256423001"
+          }
+          mountDivId="headerMain"
+          globalVarName="headerMain"
+          propsToPass={stableProps}
+        />
       </div>
-    </div>
-
-  </>
-}
-
-
-
+      <div className="flex bg-var(--color-primary)">
+        <MicrofrontendLoader
+          ref={mfRef}
+          scriptUrl={
+            "https://d18aratlqkym29.cloudfront.net/frontend-build/sidebar/1.1/mf/sidebar-bundle.js?date=1765256423001"
+          }
+          mountDivId="sidebarMain"
+          globalVarName="sidebarMain"
+          propsToPass={stableProps}
+          onLoad={() => setSidebarLoaded(true)}
+        />
+        <div
+          className={`${
+            !open ? "xl:max-w-[calc(100%-80px)]" : "xl:max-w-[calc(100%-240px)]"
+          } relative min-h-[calc(100%-80px)]! p-4 xl:p-6 pt-[104px]! w-full bg-[#F5F9FE]`}
+        >
+          {true ? (
+            <ReactRouterDOM.Outlet />
+          ) : (
+            <div className="fixed inset-0 bg-white z-40 flex items-center justify-center">
+              <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 const AppWrapper = () => {
-  return <>
-    <ReactRouterDOM.BrowserRouter>
-      <ReactRouterDOM.Routes>
-        <ReactRouterDOM.Route path='/' element={<PrimaryLayout />}>
-          <ReactRouterDOM.Route path='/*' element={<App />} />
-        </ReactRouterDOM.Route>
-      </ReactRouterDOM.Routes>
-    </ReactRouterDOM.BrowserRouter>
+  return (
+    <>
+      <ReactRouterDOM.BrowserRouter>
+        <ReactRouterDOM.Routes>
+          <ReactRouterDOM.Route path="/" element={<PrimaryLayout />}>
+            <ReactRouterDOM.Route path="/*" element={<App />} />
+          </ReactRouterDOM.Route>
+        </ReactRouterDOM.Routes>
+      </ReactRouterDOM.BrowserRouter>
+    </>
+  );
+};
 
-  </>
-}
-
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppWrapper />
-  </StrictMode>,
-)
+  </StrictMode>
+);
